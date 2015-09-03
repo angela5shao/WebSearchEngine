2cf36f4c8640771611cf49bc2b08dee77e66795d
Ann-Chie Shao <annchies@usc.edu>
2015-09-03 14:59:46 -0700

    parsemd can parse special characters and strings with just link

--- hw1/parsemd.cpp
+++ hw1/parsemd.cpp
@@ -10,15 +10,67 @@ Output: string
 */
 void parser(string str) {
 	int len = str.length();
-	//if (str == "") return; // base case
+	//cout << str << ": ";
 
+	if (str == "") return;
+
+	for (int i=0; i<len; i++) {
+		// if a special char, print what's before and call parser for what's after
+		char x = str[i];
+		
+		if (x < 'A' || (x > 'Z' && x < 'a') || x > 'z') {
+			cout << str.substr(0,i) << endl;
+			parser(str.substr(i+1, len));
+			break;
+		}
+		// if entire string are letters
+		if (i==len-1) {
+			cout << str << " (normal)" << endl;
+		}
+	}
+
+	//cout << " (end of parser)" << endl;
+
+/*
 	for (int i=0; i<len; i++) {
 		// if any char isn't a letter, print out whatever is before it and call parser for what's after
 		if (str[i] < 'a' || str[i] > 'z') {
 			cout << str.substr(0,i) << endl;
 			//parser(str.substr(i+1,len));
 		}
+	}*/
+}
+
+/* process links without anchor text; PRINTS
+Param: string, including ()
+Output: LINK (content, content)
+ */
+void outputLink(string str) {
+	int len = str.length();
+	// find ')' and output
+	for (int i=1; i<len; i++) {
+		if (str[i] == ')') {
+			string buf = str.substr(1,i-1);
+			cout << "LINK (" << buf << ", " << buf << ")";
+			// if ')' isn't end of str, call parser for what's after ')'
+			if (i != len-1) {
+				cout << "(calling parser!)" << endl;
+				parser(str.substr(i+1,len));
+			}
+		}
 	}
+}
+
+/* process links with anchor text; PRINTS
+Param: string, including [] and ()
+Output: LINK (content, anchor_text)
+*/
+void outputLinkWithAnchor(string str) {
+	// find ']' and see if '(' follows
+
+		// if '(' doesn't follow, output content inside []
+
+		// else, output
 }
 
 int main(int argc, char* argv[]) {
@@ -35,24 +87,20 @@ int main(int argc, char* argv[]) {
 		int len = buf.length();
 		//cout << buf << endl;
 
-		// if starts with '(' or '[', output what's inside
-		if (buf[0] == '(' || buf[0] == '[') {
-			// if something is after ')' or ']', output what's inside
-			for (int i=0; i<len; i++) {
-				if (buf[i] == ')' || buf[i] == ']') {
-					cout << buf.substr(1,i) << endl;
-				}
-			}
+		// if starts with '(', call outputLink
+		if (buf[0] == '(') {
+			outputLink(buf);
 		}
 
-		// else: check for non-letters
+		// else if starts with '[', call outputLinkWithAnchor
+/*
+		else if (buf[0] == '[') {
+			outputLinkWithAnchor(buf);
+		}*/
+		// else, call parser
 		else {
 			// if reach non-letter, print what's before
-			for (int i=0; i<len; i++) {
-				if (buf[i] < 'a' || buf[i] > 'z') { // ** CHECK FOR CAPITALS TOO ** */
-					cout << buf.substr(0,i) << endl;
-				}
-			}
+			parser(buf);
 		}
 
 	}
