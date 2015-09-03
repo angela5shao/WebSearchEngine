994858f8751a5ba25ac20e5ab3f53cd911d85d86
Ann-Chie Shao <annchies@usc.edu>
2015-09-03 16:20:28 -0700

    parsemd outputs parsed input to a file, currently printing out what it outputs

--- hw1/parsemd.cpp
+++ hw1/parsemd.cpp
@@ -8,54 +8,49 @@ using namespace std;
 Param: string 
 Output: string
 */
-void parser(string str) {
+void parser(string str, ofstream &output) {
 	int len = str.length();
-	//cout << str << ": ";
 
-	if (str == "") return;
+	if (str == "") return; // base case
 
 	for (int i=0; i<len; i++) {
 		// if a special char, print what's before and call parser for what's after
 		char x = str[i];
-		
 		if (x < 'A' || (x > 'Z' && x < 'a') || x > 'z') {
-			cout << str.substr(0,i) << endl;
-			parser(str.substr(i+1, len));
+			// if not first character
+			if (i != 0) {
+				cout << str.substr(0,i) << endl;
+				output << str.substr(0,i) << endl;
+			}
+			parser(str.substr(i+1, len), output);
 			break;
 		}
 		// if entire string are letters
 		if (i==len-1) {
-			cout << str << " (normal)" << endl;
+			cout << str << /*" (normal)" << */endl;
+			output << str << endl;
 		}
 	}
-
-	//cout << " (end of parser)" << endl;
-
-/*
-	for (int i=0; i<len; i++) {
-		// if any char isn't a letter, print out whatever is before it and call parser for what's after
-		if (str[i] < 'a' || str[i] > 'z') {
-			cout << str.substr(0,i) << endl;
-			//parser(str.substr(i+1,len));
-		}
-	}*/
 }
 
 /* process links without anchor text; PRINTS
 Param: string, including ()
 Output: LINK (content, content)
  */
-void outputLink(string str) {
+void outputLink(string str, ofstream &output) {
 	int len = str.length();
 	// find ')' and output
 	for (int i=1; i<len; i++) {
 		if (str[i] == ')') {
 			string buf = str.substr(1,i-1);
 			cout << "LINK (" << buf << ", " << buf << ")";
+
+			string temp = "LINK (" + buf + " " + buf + ")";
+			output << temp << endl;
+
 			// if ')' isn't end of str, call parser for what's after ')'
 			if (i != len-1) {
-				cout << "(calling parser!)" << endl;
-				parser(str.substr(i+1,len));
+				parser(str.substr(i+1,len), output);
 			}
 		}
 	}
@@ -65,12 +60,40 @@ void outputLink(string str) {
 Param: string, including [] and ()
 Output: LINK (content, anchor_text)
 */
-void outputLinkWithAnchor(string str) {
-	// find ']' and see if '(' follows
+void outputLinkWithAnchor(string str, ofstream &output) {
+	int len = str.length();
+	string anchor;
+	int indexOpenParen;
+	
+	for (int i=1; i<len; i++) {
+		// find ']' and see if '(' follows
+		if (str[i] == ']') {
+			// if followed by '(' with a link, save anchorText
+			if (str[i+1] == '(') {
+				anchor = str.substr(1,i-1);
+				indexOpenParen = i+1;
+			}
+			// else, print content
+			else {
+				cout << /*" (no link) " <<*/ str.substr(1,i-1) << endl;
+				output << str.substr(1,i-1) << endl;
+				break;
+			}
+		}
+		// find ')'
+		if (str[i] == ')') {
+			string buf = str.substr(indexOpenParen + 1, i);
+			cout << "LINK (" << buf << ", " << anchor << ")" << endl;
 
-		// if '(' doesn't follow, output content inside []
+			string temp = "LINK (" + buf + ", " + anchor + ")";
+			output << temp << endl;
 
-		// else, output
+			// if ')' isn't end of str, call parser for what's after ')'
+			if (i != len-1) {
+				parser(str.substr(i+1,len), output);
+			}
+		}
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -79,29 +102,25 @@ int main(int argc, char* argv[]) {
 	if (input.fail()) // if file fails
 		cout << "File cannot be opened" << endl;
 
-	ofstream output; // create output file
+	ofstream output; // create output file and open it
+	output.open(argv[2]);
 
 	// read in inputs
 	string buf;
 	while (input >> buf) {
-		int len = buf.length();
 		//cout << buf << endl;
 
-		// if starts with '(', call outputLink
-		if (buf[0] == '(') {
-			outputLink(buf);
+		if (buf[0] == '(') { // if starts with '(', call outputLink
+			outputLink(buf, output);
+		} else if (buf[0] == '[') { // else if starts with '[', call outputLinkWithAnchor
+			outputLinkWithAnchor(buf, output);
+		} else { // else, call parser
+			parser(buf, output);
 		}
-
-		// else if starts with '[', call outputLinkWithAnchor
-/*
-		else if (buf[0] == '[') {
-			outputLinkWithAnchor(buf);
-		}*/
-		// else, call parser
-		else {
-			// if reach non-letter, print what's before
-			parser(buf);
-		}
-
 	}
+
+	// close output file
+	output.close();
+
+	return 0;
 }
