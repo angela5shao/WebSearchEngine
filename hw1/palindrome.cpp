ad8a3866ddee0fa3f7af9c4855130bd60d62812c
Ann-Chie Shao <annchies@usc.edu>
2015-09-04 17:52:16 -0700

    palindrome: finished

--- hw1/palindrome.cpp
+++ hw1/palindrome.cpp
@@ -5,11 +5,10 @@
 
 using namespace std;
 
-void addToEnds(string str, int len);
-void makePalindromes(ostream& ofile, char* options, int size);
+void addToEnds(ofstream &ofile, string temp, string orig, int size);
+void makePalindromes(ofstream& ofile, char* options, int size);
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   if(argc < 4){
     cerr << "Please provide an output file, a string of characters, and the max length" << endl;
     return 1;
@@ -22,7 +21,7 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-void addToEnds(string temp, string orig, int size) {
+void addToEnds(ofstream& ofile, string temp, string orig, int size) {
 	int len = temp.length();
 	int lenOrig = orig.length();
 	string buf = "";
@@ -31,35 +30,36 @@ void addToEnds(string temp, string orig, int size) {
 		// for each char in str, add to both ends of str
 		for (int i=0; i<lenOrig; i++) {
 			buf = orig[i] + temp + orig[i];
-			cout << buf << endl;
+			ofile << buf << endl;
 
-			addToEnds(buf, orig, size);
+			addToEnds(ofile, buf, orig, size);
 		}
 	}
 	else return;
 }
 
-void makePalindromes(ostream& ofile, char* options, int size) {
+void makePalindromes(ofstream& ofile, char* options, int size) {
 	string orig(options);
 	int len = orig.length();
 
-	cout << "" << endl;
+	if (len == 0) {
+		ofile << orig << endl;
+	}
+	else {
+		// for each char in orig
+		for (int i=0; i<len; i++) {
+			// for strings of odd length
+			string temp1 = "";
+			ofile << temp1 << endl;
+			temp1 = temp1 + orig[i];
+			ofile << temp1 << endl;
+			addToEnds(ofile, temp1, orig, size);
 
-	// for each char in orig
-	for (int i=0; i<len; i++) {
-		// for strings of odd length
-		string temp1 = "";
-		temp1 = temp1 + orig[i];
-		cout << temp1 << endl;
-		addToEnds(temp1, orig, size);
-
-		// for strings of even length
-		//cout << ".." << endl;
-		string temp2 = "";
-		temp2 = orig[i] + temp2 + orig[i];
-		cout << temp2 << endl;
-		addToEnds(temp2, orig, size);
-
-		//cout << "... end of letter ..." << endl;
+			// for strings of even length
+			string temp2 = "";
+			temp2 = orig[i] + temp2 + orig[i];
+			ofile << temp2 << endl;
+			addToEnds(ofile, temp2, orig, size);
+		}
 	}
 }
