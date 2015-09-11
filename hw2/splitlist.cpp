c904538179e1adfd5725d8b0dd3b2c20a00ca810
Ann-Chie Shao <annchies@usc.edu>
2015-09-10 17:11:00 -0700

    started Problem 1,3,4,5

---
+++ hw2/splitlist.cpp
@@ -0,0 +1,31 @@
+/* Problem 5:  */
+
+struct Node {
+	int value;
+	Node *next;
+};
+
+/* Purpose: splits list "in" to smaller list for elements <= pivot and larger list for elements > pivot
+param: pointers to list "in", "smaller", "larger", and int pivot
+return: none
+*/
+void split (Node*& in, Node*& smaller, Node*&larger, int pivot) {
+	// BASE: at pivot Node
+	if ()
+	// traverse until pivot Node
+
+
+/* starting at head Node until Node->next = Node[pivot + 1], 
+for each Node of "in", add to smaller list (on heap)
+point to next Node of "in", delete current Node
+*/
+
+/* starting at Node[pivote + 1] until Node->next = NULL,
+for each Node of "in", add to larger list (on heap)
+point to next Node of "in", delete current Node
+*/
+
+// set list "in" to NULL
+}
+
+/** make README.md **/
