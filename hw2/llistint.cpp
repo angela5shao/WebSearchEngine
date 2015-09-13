8ea73ee82833b1920314d7332fd286e7b677e204
Ann-Chie Shao <annchies@usc.edu>
2015-09-12 21:36:23 -0700

    starting Problem 6

---
+++ hw2/llistint.cpp
@@ -0,0 +1,90 @@
+#include "llistint.h"
+#include <cstdlib>
+#include <stdexcept>
+
+LListInt::LListInt()
+{
+  head_ = NULL;
+  tail_ = NULL;
+  size_ = 0;
+}
+
+LListInt::~LListInt()
+{
+  clear();
+}
+
+bool LListInt::empty() const
+{
+  return size_ == 0;
+}
+
+int LListInt::size() const
+{
+  return size_;
+}
+
+/**
+ * Complete the following function
+ */
+void LListInt::insert(int loc, const int& val)
+{
+
+
+
+}
+
+/**
+ * Complete the following function
+ */
+void LListInt::remove(int loc)
+{
+
+
+
+}
+
+void LListInt::set(int loc, const int& val)
+{
+  Item *temp = getNodeAt(loc);
+  temp->val = val;
+}
+
+int& LListInt::get(int loc)
+{
+  if(loc < 0 || loc >= size_){
+    throw std::invalid_argument("bad location");
+  }
+  Item *temp = getNodeAt(loc);
+  return temp->val;
+}
+
+int const & LListInt::get(int loc) const
+{
+  if(loc < 0 || loc >= size_){
+    throw std::invalid_argument("bad location");
+  }
+  Item *temp = getNodeAt(loc);
+  return temp->val;
+}
+
+void LListInt::clear()
+{
+  while(head_ != NULL){
+    Item *temp = head_->next;
+    delete head_;
+    head_ = temp;
+  }
+  tail_ = NULL;
+  size_ = 0;
+}
+
+
+/**
+ * Complete the following function
+ */
+LListInt::Item* LListInt::getNodeAt(int loc) const
+{
+
+
+}
